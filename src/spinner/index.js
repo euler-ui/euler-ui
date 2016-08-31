0ace17d4e5e313429df61bcac1113cf09c177266
奚金鑫 <jinshinxi@gmail.com>
2016-08-31 17:50:26 +0800

    Circle Spinner style enhancement

--- src/spinner/index.js
+++ src/spinner/index.js
@@ -20,26 +20,27 @@ const DotLoader = React.createClass({
 const CircleLoader = React.createClass({
   componentDidMount() {
     var opts = {
-      lines: 13, // The number of lines to draw
-      length: 12, // The length of each line
-      width: 10, // The line thickness
-      radius: 20, // The radius of the inner circle
+      lines: 12, // The number of lines to draw
+      length: 10, // The length of each line
+      width: 8, // The line thickness
+      radius: 22, // The radius of the inner circle
       scale: 1, // Scales overall size of the spinner
       corners: 1, // Corner roundness (0..1)
-      color: '#000', // #rgb or #rrggbb or array of colors
-      opacity: 0.25, // Opacity of the lines
+      color: '#222', // #rgb or #rrggbb or array of colors
+      opacity: 0.2, // Opacity of the lines
       rotate: 0, // The rotation offset
       direction: 1, // 1: clockwise, -1: counterclockwise
       speed: 1, // Rounds per second
       trail: 60, // Afterglow percentage
       fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
       zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: '40%', // Top position relative to parent
+      className: 'inner', // The CSS class to assign to the spinner
+      top: '48%', // Top position relative to parent
       left: '50%', // Left position relative to parent
       shadow: false, // Whether to render a shadow
       hwaccel: false, // Whether to use hardware acceleration
       position: 'absolute' // Element positioning
-    }
+    };
     var target = ReactDOM.findDOMNode(this.refs.spin);
     new CircleSpinner(opts).spin(target);
   },
